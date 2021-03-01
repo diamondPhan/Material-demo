95bd507c1ad3db4b63b3615d6131457e561e2eab
Kim Cuong <phanngockimcuong1111@gmail.com>
2021-03-01 16:02:30 +0700

    material demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-reddit';
+
+
+  addArticle(title:HTMLInputElement,link:HTMLInputElement)
+  {
+    console.log(title.value);
+    console.log(link.value);
+    console.log('Adding article title: '+title.value +'and link '+link.value);
+    return false;
+  }
 }
